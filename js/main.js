a7b8e348d51c380f94b7f532f589849ac30ee202
Elmira <fareli_n@yahoo.com>
2024-09-24 17:42:13 -0700

    Build about component.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,20 @@
-'use strict'
-const collapsible = document.querySelectorAll('.collapsible')
-collapsible.forEach (element => {
-    element.addEventListener('click' , toggle)
-})
-function toggle(){
-    this.classList.toggle("collapsible--expanded");
+"use strict";
+const collapsible = document.querySelectorAll(".collapsible");
+collapsible.forEach((element) => {
+  element.addEventListener("click", toggleExpand);
+});
+function toggleExpand() {
+  this.classList.toggle("collapsible--expanded");
+}
+
+const readMore = document.querySelector(".read-more");
+const readMoreContent = document.querySelector(".read-more-content");
+readMore.addEventListener("click", toggleReadMore);
+function toggleReadMore() {
+  readMoreContent.classList.toggle("show-read-more-content");
+  if (readMore.innerText.toLowerCase().includes("more")) {
+    readMore.innerText = "Read Less ...";
+  } else {
+    readMore.innerText = "Read More ...";
+  }
 }
